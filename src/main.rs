1ff1407020099e59c103851e3ee2203c3a8c5334
nmktkm <T.Namiki@uec.ac.jp>
2024-04-28 12:18:05 +0900

    dekita

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     #[cfg(feature = "dhat-heap")]
     let _dhat = dhat::Profiler::new_heap();
     let encryption = AES128::new_from_str("Tachibana_Hinano");
-    let data = vec![0x00u8; 1<<4];
+    let data = vec![0x00u8; 1<<24];
     let now = time::Instant::now();
     let cipher = (encryption.encrypt)(&encryption, &data);
     println!("{:?}", now.elapsed());
